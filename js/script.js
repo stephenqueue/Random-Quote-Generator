663764792799b70779a30df0ebeb3d290e91f35b
Kellen <Kellenkjames@gmail.com>
2017-12-04 09:27:48 -0800

    updated values for quotes array

--- js/script.js
+++ js/script.js
@@ -2,70 +2,69 @@
 var quotes = [
   {
     quote: "The Way To Get Started Is To Quit Talking And Begin Doing.",
-    source: " - Walt Disney",
-    citation: "",
-    year: ""
+    source: "Walt Disney",
+    citation: "Publication",
+    year: "1950"
   },
   {
     quote: "The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.",
-    source: " - Winston Churchill",
-    citation: "",
-    year: ""
+    source: "Winston Churchill",
+    citation: "Unknown",
+    year: "Unknown"
   },
   {
     quote: "Don’t Let Yesterday Take Up Too Much Of Today.",
-    source: " - Will Rogers",
-    citation: "",
-    year: ""
+    source: "Will Rogers",
+    citation: "Publication",
+    year: "1915"
   },
   {
     quote: "You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.",
-    source: " - Unknown",
-    citation: "",
-    year: ""
+    source: "Unknown",
+    citation: "Unknown",
+    year: "Unknown"
   },
   {
     quote: "If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.",
-    source: " - Steve Jobs",
-    citation: "",
-    year: ""
+    source: "Steve Jobs",
+    citation: "Article",
+    year: "2004"
   },
   {
     quote: "People Who Are Crazy Enough To Think They Can Change The World, Are The Ones Who Do.",
-    source: " - Rob Siltanen",
-    citation: "",
-    year: ""
+    source: "Rob Siltanen",
+    citation: "Article",
+    year: "1970"
   },
   {
     quote: "Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough.",
-    source: " - Og Mandino",
-    citation: "",
-    year: ""
+    source: "Og Mandino",
+    citation: "Publication",
+    year: "Unknown"
   },
   {
-    quote: "Failure Will Never Overtake Me If My Determination To Succeed Is Strong Enough.",
-    source: " - Og Mandino",
-    citation: "",
-    year: ""
+    quote: "If you don’t make the time to work on creating the life you want, you’re eventually going to be forced to spend a LOT of time dealing with a life you DON’T want.",
+    source: "Kevin Ngo",
+    citation: "Book",
+    year: "2013"
   },
   {
     quote: "Knowing Is Not Enough; We Must Apply. Wishing Is Not Enough; We Must Do.",
-    source: " - Johann Wolfgang Von Goethe",
-    citation: "",
-    year: ""
+    source: "Johann Wolfgang Von Goethe",
+    citation: "Article",
+    year: "1875"
   },
   {
     quote: "The Man Who Has Confidence In Himself Gains The Confidence Of Others.",
-    source: " - Hasidic Proverb",
-    citation: "",
-    year: ""
+    source: "Hasidic Proverb",
+    citation: "Publication",
+    year: "2016"
   },
 
 ]; //end of quotes array
 
 //Global Variables
 var print;
-var quotesLoop;
 
 //selects a random quote from the array and returns a value
 function getRandomQuote() {
@@ -78,18 +77,19 @@ function printQuote() {
   print = getRandomQuote();
   console.log(print);
 
-//for in loop to iterate through each object in the array
+  //for in loop to iterate through each object in the array
   for (var i = 0; i < quotes.length; i++) {
-    var quotesLoop = quotes[i];
 
-//displays final HTML string to the page
+    //displays final HTML string to the page
     document.getElementById('quote-box').innerHTML =
     '<p class = "quote">' + print.quote + '</p>'+
     '<p class = "source">' + print.source + '</p>'+
     '<span class = "citation">' + print.citation + '</span>'+
     '<span class = "year">' + print.year + '</span>';
-    if(print.citation === "" || print.year === "") {
+
+    if (print.citation === "Unknown" || print.year === "Unknown") {
       break;
+
     } //end of conditional statement
   } //end for loop
 } // end printQuote function
